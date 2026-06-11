39f1c1e800184458b59bc28b7d7159096078310b
Occumed79 <Alex.Ayvazian@logan.edu>
2026-06-11 10:25:20 -0700

    Wire prospect intelligence route

--- artifacts/occu-med-insight-hub/src/App.tsx
+++ artifacts/occu-med-insight-hub/src/App.tsx
@@ -8,6 +8,7 @@ import Landing from "@/pages/landing";
 import DataProfiles from "@/pages/data-profiles";
 import QuantifiableData from "@/pages/quantifiable-data";
 import GeographicData from "@/pages/geographic-data";
+import ProspectIntelligence from "@/pages/prospect-intelligence";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ function Router() {
       <Route path="/data-profiles" component={DataProfiles} />
       <Route path="/quantifiable-data" component={QuantifiableData} />
       <Route path="/geographic-data" component={GeographicData} />
+      <Route path="/portal/prospects" component={ProspectIntelligence} />
       <Route component={NotFound} />
     </Switch>
   );
