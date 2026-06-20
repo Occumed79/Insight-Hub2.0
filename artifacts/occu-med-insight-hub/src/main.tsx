80ad9a07e2b93926d2e656334291101cce7722b7
Occumed79 <Alex.Ayvazian@logan.edu>
2026-06-20 15:18:25 -0700

    Load restored visual effects

--- artifacts/occu-med-insight-hub/src/main.tsx
+++ artifacts/occu-med-insight-hub/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./visual-restore.css";
 import "./logo-override.css";
 import "leaflet/dist/leaflet.css";
 import "./map-polish.css";
