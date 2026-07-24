9d73e14d4137140ed69b1dbace7f48a5f50a4efe
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-24 14:03:57 -0700

    Load Locations map color treatment

--- artifacts/occu-med-insight-hub/src/main.tsx
+++ artifacts/occu-med-insight-hub/src/main.tsx
@@ -3,5 +3,6 @@ import App from "./App";
 import "./styles/foundation.css";
 import "leaflet/dist/leaflet.css";
 import "./styles/application.css";
+import "./styles/locations-map.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
