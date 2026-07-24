ada4c9e53eb548a5234d272d7cf93bc3d2dfa9a5
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-23 19:13:19 -0700

    Load cardless tool surface styles

--- artifacts/occu-med-insight-hub/src/main.tsx
+++ artifacts/occu-med-insight-hub/src/main.tsx
@@ -3,5 +3,6 @@ import App from "./App";
 import "./styles/foundation.css";
 import "leaflet/dist/leaflet.css";
 import "./styles/application.css";
+import "./styles/cardless-tools.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
