64d496f7b09e0e4351eb2d4c521fd977440734a3
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-23 19:16:21 -0700

    Remove temporary cardless stylesheet import

--- artifacts/occu-med-insight-hub/src/main.tsx
+++ artifacts/occu-med-insight-hub/src/main.tsx
@@ -3,6 +3,5 @@ import App from "./App";
 import "./styles/foundation.css";
 import "leaflet/dist/leaflet.css";
 import "./styles/application.css";
-import "./styles/cardless-tools.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
