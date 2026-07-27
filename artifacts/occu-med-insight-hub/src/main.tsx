bcec013539b45305ccf9c6036c6fab57ce65f81b
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-27 10:34:28 -0700

    Load scoped translucent tool styling

--- artifacts/occu-med-insight-hub/src/main.tsx
+++ artifacts/occu-med-insight-hub/src/main.tsx
@@ -3,5 +3,6 @@ import App from "./App";
 import "./styles/foundation.css";
 import "leaflet/dist/leaflet.css";
 import "./styles/application.css";
+import "./styles/translucent-tool-panels.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
