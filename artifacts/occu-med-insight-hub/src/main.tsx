d815ab19f7126120303c1307fe791a4ba75dc31e
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-29 11:46:56 -0700

    Always open Data Profiles at the company library

--- artifacts/occu-med-insight-hub/src/main.tsx
+++ artifacts/occu-med-insight-hub/src/main.tsx
@@ -4,4 +4,8 @@ import "./styles/foundation.css";
 import "leaflet/dist/leaflet.css";
 import "./styles/application.css";
 
+if (window.location.pathname.replace(/\/$/, "").endsWith("/data-profiles")) {
+  sessionStorage.removeItem("insight-hub.company-library.selected");
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
