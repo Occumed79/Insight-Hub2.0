249548f6d926ba80553e2a33a39e6706c9551374
Occumed79 <Alex.Ayvazian@logan.edu>
2026-08-06 20:55:29 -0700

    Use audited application style aggregator

--- artifacts/occu-med-insight-hub/src/main.tsx
+++ artifacts/occu-med-insight-hub/src/main.tsx
@@ -2,6 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/foundation.css";
 import "leaflet/dist/leaflet.css";
-import "./styles/application.css";
+import "./styles/app-entry.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
