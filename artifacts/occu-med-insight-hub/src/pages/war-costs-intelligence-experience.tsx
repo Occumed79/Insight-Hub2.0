73c1249c0d0678468d4497ebb4b730d6e792ec98
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-21 13:11:19 -0700

    Use approved DBA aurora shell on workspace

--- artifacts/occu-med-insight-hub/src/pages/war-costs-intelligence-experience.tsx
+++ artifacts/occu-med-insight-hub/src/pages/war-costs-intelligence-experience.tsx
@@ -3,5 +3,5 @@ import "./war-costs-intelligence-experience.css";
 import "../styles/glass-recovery.css";
 
 export default function WarCostsIntelligenceExperience() {
-  return <div className="war-costs-intelligence-experience" data-workspace="defense-overview"><WarCostsIntelligence /></div>;
+  return <div className="war-costs-intelligence-experience aurora-bg" data-workspace="defense-overview"><WarCostsIntelligence /></div>;
 }
