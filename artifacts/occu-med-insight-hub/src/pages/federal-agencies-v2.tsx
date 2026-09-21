79117a369365cb198822d6e67139216a2b6e97c4
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-21 10:49:23 -0700

    Route Federal Agencies proof to cinematic workspace

--- artifacts/occu-med-insight-hub/src/pages/federal-agencies-v2.tsx
+++ artifacts/occu-med-insight-hub/src/pages/federal-agencies-v2.tsx
@@ -1,10 +1,5 @@
-import LegacyFederalAgencies from "./federal-agencies-v2-legacy";
-import "./federal-agencies-workbench.css";
+import FederalAgenciesCinematic from "./federal-agencies-cinematic";
 
 export default function FederalAgenciesV2() {
-  return (
-    <div className="federal-agencies-workbench-v2" data-workbench="federal-agency-intelligence">
-      <LegacyFederalAgencies />
-    </div>
-  );
+  return <FederalAgenciesCinematic />;
 }
