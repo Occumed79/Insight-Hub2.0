e1f56b6feb8f3c8054773b55257bfed00f41ac45
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-21 13:11:07 -0700

    Use approved DBA aurora shell on workspace

--- artifacts/occu-med-insight-hub/src/pages/industry-impact-experience.tsx
+++ artifacts/occu-med-insight-hub/src/pages/industry-impact-experience.tsx
@@ -3,7 +3,7 @@ import "./industry-impact-experience.css";
 
 export default function IndustryImpactExperience() {
   return (
-    <div className="industry-impact-experience" data-workspace="industry-impact">
+    <div className="industry-impact-experience aurora-bg" data-workspace="industry-impact">
       <div className="industry-impact-field" aria-hidden="true">
         <span className="impact-horizon impact-horizon-one" />
         <span className="impact-horizon impact-horizon-two" />
