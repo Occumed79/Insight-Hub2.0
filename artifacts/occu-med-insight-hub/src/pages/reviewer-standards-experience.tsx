5c6af67016866f34c49c1b3e3ef4d2b4a8ad8d67
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-21 13:10:43 -0700

    Use approved DBA aurora shell on workspace

--- artifacts/occu-med-insight-hub/src/pages/reviewer-standards-experience.tsx
+++ artifacts/occu-med-insight-hub/src/pages/reviewer-standards-experience.tsx
@@ -3,7 +3,7 @@ import "./reviewer-standards-experience.css";
 
 export default function ReviewerStandardsExperience() {
   return (
-    <div className="standards-experience" data-workspace="standards-intelligence">
+    <div className="standards-experience aurora-bg" data-workspace="standards-intelligence">
       <div className="standards-rule-field" aria-hidden="true">
         <span className="standards-rule-line standards-rule-line-a" />
         <span className="standards-rule-line standards-rule-line-b" />
