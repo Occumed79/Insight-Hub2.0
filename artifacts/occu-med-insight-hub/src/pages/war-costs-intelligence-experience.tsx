25029991fe3f955a0c235085042c14f2ee4d7db9
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-21 12:15:57 -0700

    Apply glass recovery after route styles

--- artifacts/occu-med-insight-hub/src/pages/war-costs-intelligence-experience.tsx
+++ artifacts/occu-med-insight-hub/src/pages/war-costs-intelligence-experience.tsx
@@ -1,5 +1,6 @@
 import WarCostsIntelligence from "./war-costs-intelligence";
 import "./war-costs-intelligence-experience.css";
+import "../styles/glass-recovery.css";
 
 export default function WarCostsIntelligenceExperience() {
   return <div className="war-costs-intelligence-experience" data-workspace="defense-overview"><WarCostsIntelligence /></div>;
