a724b7bae3acb2257a2eff27ac51a4ec61dee446
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-21 12:32:34 -0700

    Remove temporary glass recovery import

--- artifacts/occu-med-insight-hub/src/pages/reviewer-drug-checker-experience.tsx
+++ artifacts/occu-med-insight-hub/src/pages/reviewer-drug-checker-experience.tsx
@@ -1,6 +1,5 @@
 import ReviewerDrugChecker from "./reviewer-drug-checker";
 import "./reviewer-drug-checker-experience.css";
-import "../styles/glass-recovery.css";
 
 export default function ReviewerDrugCheckerExperience() {
   return (
