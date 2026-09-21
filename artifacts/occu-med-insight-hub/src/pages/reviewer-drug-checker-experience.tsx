1502704ec039ee1463bcef6c1413c1dacc22797b
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-21 13:10:49 -0700

    Use approved DBA aurora shell on workspace

--- artifacts/occu-med-insight-hub/src/pages/reviewer-drug-checker-experience.tsx
+++ artifacts/occu-med-insight-hub/src/pages/reviewer-drug-checker-experience.tsx
@@ -3,7 +3,7 @@ import "./reviewer-drug-checker-experience.css";
 
 export default function ReviewerDrugCheckerExperience() {
   return (
-    <div className="drug-checker-experience" data-workspace="drug-checker">
+    <div className="drug-checker-experience aurora-bg" data-workspace="drug-checker">
       <div className="drug-spectrum-field" aria-hidden="true">
         <span className="drug-spectrum-line drug-spectrum-line-a" />
         <span className="drug-spectrum-line drug-spectrum-line-b" />
