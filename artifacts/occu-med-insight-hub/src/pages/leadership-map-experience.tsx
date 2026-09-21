4f5f3d5212f22c018c69d526770d832a98a50014
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-21 14:12:24 -0700

    Purge visible UI from leadership-map-experience

--- artifacts/occu-med-insight-hub/src/pages/leadership-map-experience.tsx
+++ artifacts/occu-med-insight-hub/src/pages/leadership-map-experience.tsx
@@ -1,17 +1,10 @@
-import LeadershipMap from "./leadership-map";
-import "./leadership-map-experience.css";
+import RebuildHoldWorkspace from "./rebuild-hold-workspace";
 
 export default function LeadershipMapExperience() {
   return (
-    <div className="leadership-map-experience aurora-bg" data-workspace="organizational-chart">
-      <div className="leadership-topology-field" aria-hidden="true">
-        <span className="leadership-orbit leadership-orbit-one" />
-        <span className="leadership-orbit leadership-orbit-two" />
-        <span className="leadership-node leadership-node-one" />
-        <span className="leadership-node leadership-node-two" />
-        <span className="leadership-node leadership-node-three" />
-      </div>
-      <LeadershipMap />
-    </div>
+    <RebuildHoldWorkspace
+      workspace="organizational-chart"
+      identityKey="organizational-chart"
+    />
   );
 }
